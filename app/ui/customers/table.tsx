16702b18772f3cf2f9b5137eb4a5fb267c6d5495
Olamiposi <tunde-ajayi.olaoluwasijibomi@lmu.edu.ng>
2024-06-01 07:10:57 +0100

    Authentication

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -1,16 +1,18 @@
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
 import Search from '@/app/ui/search';
-import {
-  CustomersTableType,
-  FormattedCustomersTable,
-} from '@/app/lib/definitions';
+import { FormattedCustomersTable } from '@/app/lib/definitions';
+import { fetchFilteredCustomers } from '@/app/lib/data';
 
 export default async function CustomersTable({
-  customers,
-}: {
-  customers: FormattedCustomersTable[];
+  query, 
+  currentPage,
+}:{
+  query: string; 
+  currentPage: number;
 }) {
+  const customers: FormattedCustomersTable[] = await fetchFilteredCustomers(query, currentPage);
+
   return (
     <div className="w-full">
       <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
